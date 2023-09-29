baffcf34267c4d78b96bdf80654934427d98e8fe
Faezeh <79566997+BOSHRAA135@users.noreply.github.com>
2023-09-30 00:06:10 +0330

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -715,3 +715,495 @@ for(var i = 1 ; i <= 5 ; i++){
     str = str + "\n";
 }
 console.log(str);
+// Array
+var a = 20;
+console.log(typeof a);
+var b = "javaScript";
+console.log(typeof b);
+var c = [12 , 14 ,16 ,'javascript'];
+console.log(typeof c );
+console.log(c);
+
+var d = [];
+console.log(d);
+
+d[0]= 10;
+d[1]= 20;
+d[2]= "javascript";
+console.log(d);
+
+var t = [];
+t [3]= 'javascript';
+t [0] = 'java';
+console.log(t);
+console.log("-----------------");
+
+var u = [1, 'java' , 3 , true];
+console.log(u);
+console.log(u.length); 
+
+u.length = 5;
+console.log(u);
+
+var s = [1, 'java' , 3];
+var k = [1, 'java' , 3];
+
+console.log(s == k); 
+console.log( s[0] == k[0] );
+
+//Example 8
+var grade1 = 14.5;
+var grade2 = 18;
+var grade3 = 17;
+var grade4 = 16.75;
+var grade5 = 20; 
+var sum = grade1 + grade2 + grade3 + grade4+ grade5;
+var avg = sum / 5;
+console.log(avg);
+
+var grades =[ 14.5 , 18 , 17 , 16.75 , 20];
+var sum = grades[0]+ grades[1]+ grades[2]+ grades[3] + grades[4];
+var average = sum / grades.length;
+console.log(average);
+
+var num =[1 , 2, 3, 4 ,5];
+var sum = 0 ;
+for (var i = 0 ; i<5 ; i++){
+    sum = sum + num[i];
+}
+var av = sum / num.length;
+console.log(av);
+
+
+var num7 = [17 , 20 , 24 , 31 ,44];
+for (var i = 0 ; i<num7.length ; i++){
+    if (num7[i]%2 == 0){
+        console.log(num7[i]);
+    }
+}
+
+var grade = [14.5,18,17,16.75,15,12,19,18.25,10,20];
+var sum = 0;
+for(var i = 0 ; i<grade.length ; i++){
+    sum = sum + grade[i];
+}
+var avg = sum / grade.length;
+console.log(avg);
+
+var array1 = [1 , true , 'javascript', 13.4];
+console.log(array1.toString());
+console.log(array1.join(' * '));
+console.log('-----------------------');
+
+var x = array1.toString();
+console.log(x);
+
+console.log(array1.pop());
+array1.pop();
+console.log(array1);
+console.log(array1.pop());
+console.log(array1); 
+array1.push('push');
+console.log(array1); 
+console.log(array1.push());
+var l = array1.push(2);
+console.log(l);
+console.log(array1);
+
+console.log('--------------');
+var array2=[1 , true, 'javascript', 20];
+var x = array2.shift();
+console.log(array2);
+console.log(x);
+console.log(array2.shift());
+console.log(array2);
+console.log('0------------------');
+var x = array2.unshift(5);
+console.log(array2);
+console.log(array2.unshift());
+console.log(x);
+
+var array3=[1 , true , 'javascript', 20];
+array3.unshift('java');
+console.log(array3);
+console.log(array3.length);
+
+array3[array3.length] = 'Milad';
+console.log(array3);
+
+var array4=[1 , true , 'javascript', 20];
+delete array4[1];
+console.log(array4);
+
+var str1 = "javaScript"; 
+var str2 = "java";
+console.log(str1 + str2); // concatenation
+
+var array5 = [1,2,3];
+var array6 = [4,5,6];
+var array8 = [7,8,9];
+
+var array7 = array5.concat(array6 , array8);
+console.log(array7);
+
+var array9 = [1, true , 2 , 3 , 'milad'];
+//array9.splice(0);
+var x = array9.splice(1 , 2 ,'next','code');
+console.log(array9); 
+console.log(x);
+
+var array10 = [1, true , 2 , 3 , 'milad'];
+var z = array10.slice(2 , 4);
+console.log(array10);
+console.log(z);
+
+//sort()
+var array11 = ['milad','ali','reza','Fatemeh','babak'];
+console.log(array11);
+array11.sort();
+console.log(array11);
+
+var array12 = [100 , 25, 35, 120];
+console.log(array12);
+
+array12.sort();
+console.log(array12);
+
+var array13 =[1 ,'javascript', true, 20];
+console.log(array13);
+
+array13.reverse();
+console.log(array13);
+
+var array13 =[1 ,'javascript', true, 20,'javascript', true, 20];
+console.log(array13);
+console.log(array13.indexOf('ali'));
+console.log(array13.indexOf(20));
+console.log(array13.lastIndexOf(20));
+console.log(array13.lastIndexOf('ali'));
+
+var array15 = [14,14.5,16,18,9.75,17,15];
+var max = 0;
+var min = max ; 
+for(var i = 0 ; i < array15.length ; i++){
+    if( max < array15[i]){
+        max = array15[i];  
+    }
+}
+console.log(max);
+
+var min = max ; 
+for(var i = 0 ; i < array15.length ; i++){
+    if (min > array15[i]){
+        min = array15[i];
+    }
+}
+console.log(min);
+var array16 =[1 ,'javascript', ['java',['javaF','code']], 20];
+console.log(array16);
+console.log(array16[2][0]);
+console.log(array16[2][1]);
+console.log(array16[2][1][1]);
+
+// console.log('first name = Sara');
+// console.log('last name = Rahimi');
+// console.log('age = 26');
+
+// for(var i = 1 ; i < 3 ; i++){
+//     console.log('first name = Sara');
+//     console.log('last name = Rahimi');
+//     console.log('age = 26');
+// }
+function showBio(){
+    console.log('first name = Sara');
+    console.log('last name = Rahimi');
+    console.log('age = 26');
+}
+
+showBio();
+console.log("---------------------");
+
+var Bio1 = function(){
+    console.log('first name = Sara');
+    console.log('last name = Rahimi');
+    console.log('age = 26');
+}
+
+Bio1();
+
+//Example 9
+function addNumber(num1 , num2){
+    console.log(num1 + num2);
+}
+
+addNumber(5 ,  10);
+addNumber(100 , 10);
+addNumber(300 , 50 );
+
+var addNumber1 = function(num3 , num4){
+    console.log(num3 + num4);
+}
+
+addNumber1(20 , 20);
+
+function add(num1 , num2){
+    return num1 + num2; 
+}
+var x = add(4 , 8);
+console.log(x);
+
+var add1 = function(num4 , num5 ){
+    return num4 + num5;
+}
+var t = add1(9 , 9);
+console.log(t);
+//Example 10
+var num1 = 10;
+var num2 = 5;
+var num3 = 12;
+var num4 = 8;
+var num5 = 16;
+
+var total = num1 + num2 + num3 + num4 + num5;
+var av = total/5;
+
+if(av >= 12){
+    console.log("accepted");
+}
+else{
+    console.log("conditionally");
+}
+console.log("------------------------");
+
+
+function average1(num1 , num2 , num3 , num4 , num5){
+    var total = num1 + num2 + num3 + num4 + num5;
+    var av = total/5;
+
+    if(av >= 12){
+        return "accepted";
+    }
+    else{
+        return "conditionally";
+    }
+}
+
+console.log(average1(10 , 11 , 12, 10.5 , 11.75));
+console.log(average1(12,15,18,20,19));
+var state = average1(14 ,16, 18,19,11);
+console.log(state);
+console.log("----------------");
+
+var average2 = function(num1 , num2 , num3 , num4 ,num5){
+    var total = num1 + num2 + num3 + num4 + num5;
+    var av = total/5;
+
+    if (av >= 12){
+        return "accepted";
+    }
+    else{
+        return "conditionally";
+    }
+}
+
+var state2 = average2(11 , 12, 17,10, 6);
+console.log(state2);
+
+function sum1(num1 , num2 , num3){
+  //  var total = num1 + num2 + num3;
+    return num1 + num2 + num3;
+}
+console.log(sum1(1,2,3));
+console.log(sum1(10,5 ,3));
+console.log(sum1(10 , 5));
+console.log(sum1(5 , 15 ,3 ,2));
+console.log('------------------');
+
+function myFunction(){
+    console.log(arguments.length);
+}
+myFunction();
+myFunction(10);
+myFunction(10,6,4,19);
+console.log('----------');
+
+function sum2(){
+    var total = 0;
+    for(var i = 0 ; i < arguments.length; i++){
+        total += arguments[i];
+        // total = total + arguments[i]
+    }
+    return total;
+}
+console.log(sum2());
+console.log(sum2(5));
+console.log(sum2(5 , 10));
+console.log(sum2(7 , 8 ,5 ,40 , 40));
+
+var a = [ 13, 12,13];
+console.log(a);
+
+function myFunction1(){
+    console.log(arguments);
+}
+myFunction1(5);
+
+function myFunction2(...numbers){
+    console.log(numbers);
+}
+myFunction2();
+myFunction2(6);
+myFunction2(10,8,13,2);
+
+function myfunction3(...Numbers){ //rest 
+    var total = 0;
+    for(var i = 0 ; i < Numbers.length ;i++){
+        total += Numbers[i];
+    }
+    return total;
+}
+console.log(myfunction3(1 ,2,3));
+console.log(myfunction3(5 , 20));
+
+function Sum(num1 , num2 , num3 =0){
+    return num1 + num2 + num3;
+}
+console.log(Sum(10,4,5));
+console.log(Sum(10,4));
+
+var result = 1;
+for (var i = 1 ; i <=3 ; i++){
+    result *= 3;
+}
+console.log(result);
+function power(base , exponent){
+    var result = 1;
+    for (var i = 1 ; i <=exponent ; i++){
+        result *= base;
+    }
+    return result;
+}
+console.log(power(2,0));
+
+var count = 0; num = 11;
+for(var i = 1 ; i <= num ; i++){
+    if (num % i == 0){
+        count++;
+    }
+}
+if (count == 2 ){
+    console.log("yes");
+}
+else{
+    console.log("No");
+}
+console.log(Number.isInteger(5));
+console.log(Number.isInteger(5.7));
+console.log("-------------------");
+function isPrime(num){
+    var count = 0;
+    for(var i = 1 ; i <= num ; i++){
+        if (num % i == 0)
+            count++;
+    }
+    if (count == 2 )
+        return true;
+    else
+        return false;
+}
+console.log(isPrime(10));
+console.log(isPrime(11));
+console.log(isPrime(9));
+console.log('----------------------------------------------------');
+
+function isPrime(num){
+    for(var i = 2 ; i < num; i++){
+        if (num % i == 0)
+            return false;
+    }
+    return true;   
+}
+
+console.log(isPrime(10));
+console.log(isPrime(11));
+console.log('--------------------------------------------------------');
+
+function rangePrime(firstnum , lastnum){
+    var result = [];
+    for(var i = firstnum; i <=lastnum ;i++){
+         if(isPrime(i)==true)
+            result.push(i);
+    }
+    return result;
+}
+
+console.log( rangePrime(12 , 20));
+console.log(rangePrime(50 , 100));
+console.log(rangePrime(150 , 200));
+
+console.log("----------------------------------------------------------------");
+var result =[];
+for(var i = 12 ; i<= 20;i++){
+    var count = 0;
+    for(var j = 1 ; j <= i ; j++ ){
+        if(i % j == 0)
+        count++;
+    }
+    if(count == 2)
+        result.push(i);
+}
+console.log(result);
+console.log("----------------------------------------------------------------");
+//function Declaration 
+function message(){
+    console.log("Javascript");
+} 
+// function Expression
+var message1 = function(){
+    console.log("JavaScript");
+}
+//Arrow function expressions
+var message1 = () => console.log("javaScript");
+message1();
+
+var sum5 = function(num1 , num2){
+    return num1 + num2;
+}
+
+var sum6 = (num1 , num2) => num1 + num2;
+console.log(sum6(5,10));
+
+var power1 = function(num1){
+    return num1 * num1;
+}
+
+var power2 = (num1) => num1 * num1; 
+console.log(power2(10));
+console.log(power2(9));
+
+//var isPrime = function(num){
+//     for(var i = 2 ; i < num; i++){
+//         if (num % i == 0)
+//             return false;
+//     }
+//     return true;   
+// }
+
+var isPrime = (num) => {
+    for(var i = 2 ; i < num; i++){
+        if (num % i == 0)
+            return false;
+    }
+    return true; 
+}
+//---------------------------------------------------
+function firstfunction(callback){
+    console.log("first function");
+    callback();
+}
+
+firstfunction(secondFunction);
+
+function secondFunction(){
+    console.log("second function");
+}
