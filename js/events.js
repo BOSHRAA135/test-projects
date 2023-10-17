989c5aceae85310bc0df3c2342fdb8bca3d0f52c
Faezeh <79566997+BOSHRAA135@users.noreply.github.com>
2023-10-17 23:26:50 +0330

    2 changed files

--- js/events.js
+++ js/events.js
@@ -1,1 +1,30 @@
-document.body.style.backgroundColor = 'black';
+//document.body.style.backgroundColor = 'black';
+function eventHandler(){
+    document.body.style.backgroundColor = "pink";
+}
+let button1 = document.querySelector('#button1');
+button1.onclick = function(){
+    document.body.style.backgroundColor = "purple";
+}
+button1.onclick = function(){
+    console.log('javascript');
+}
+button1.onclick = function(){
+    console.log('Hello World');
+}
+let button2 = document.querySelector('#button2');
+// button2.addEventListener('click',changeeColor);
+// function changeeColor(){
+//     document.body.style.backgroundColor = 'yellow';
+// } 
+button2.addEventListener('click',function(){
+    document.body.style.backgroundColor = 'red';
+});
+button2.addEventListener('click',function(){
+    console.log('Script');
+});
+button2.addEventListener('click',function(){
+    console.log('code');
+});
+
+// button2.addEventListener('click',() => document.body.style.backgroundColor = 'orange');
